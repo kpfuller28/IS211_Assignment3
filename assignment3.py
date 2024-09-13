8d81be1c56d77929cb5471a3b514a8ee52b8763c
kpfuller28 <kpfuller28@gmail.com>
2024-09-12 20:49:55 -0400

    Full assignment should be finished, including extra credit

--- assignment3.py
+++ assignment3.py
@@ -3,6 +3,7 @@ import csv
 import urllib.request
 import io
 import re
+import datetime
 
 # other imports go here
 
@@ -24,7 +25,62 @@ def downloadData(url):
 
 def processData(file):
     data = csv.reader(io.StringIO(file))
-    return list(data)
+
+    # Set regex patterns to find
+    filePattern = re.compile(r"\.(jpg|gif|png)$", re.IGNORECASE)
+    browserPattern = re.compile(
+        r"(Firefox|MSIE|Trident|Chrome|(?<!Chrome)Safari)", re.IGNORECASE
+    )
+
+    # Define variables needed: image hits, total hits, browser count obj
+    imageHits = 0
+    totalHits = 0
+    browserCount = {"Firefox": 0, "Internet Explorer": 0, "Chrome": 0, "Safari": 0}
+
+    # Define and initialize obj for tracking number of hits for each hour
+    hitsByHour = {}
+    for hour in range(24):
+        hitsByHour[hour] = 0
+    # Loop through all csv data
+    for hit in data:
+
+        # Find how many hits are images
+        totalHits+=1
+        if filePattern.search(hit[0]):
+            imageHits+=1
+
+        # Find which browser is used the most
+        userAgentString = hit[2]
+        browser = browserPattern.search(userAgentString).group()
+        if browser == "Firefox":
+            browser = "Firefox"
+            browserCount[browser] += 1
+        elif browser == "MSIE" or browser == "Trident":
+            browser = "Internet Explorer"
+        elif browser == "Chrome":
+            browser = "Chrome"
+        elif browser == "Safari":
+            browser = "Safari"
+        browserCount[browser] += 1
+
+        # Find how many hits each hour has
+        hitDate = datetime.datetime.strptime(hit[1], "%Y-%m-%d %H:%M:%S")
+        hitsByHour[hitDate.hour] +=1
+
+    # Last calculations: percentage of hits that are images, getting the actual most popular browser, sort hits by hour
+    imagePercentage = imageHits / totalHits * 100
+    mostPopular = max(browserCount, key=browserCount.get)
+    sortedHitsByHour = sorted(hitsByHour.items(), key=lambda i: i[1], reverse=True)
+
+    # Print everything
+    print(f"Image requests account for {imagePercentage}% of all requests")
+    print(
+        f"The most popular browser is {mostPopular} with {max(browserCount.values())} hits"
+    )
+    for hour in sortedHitsByHour:
+        if hour[1]:
+            print(f'Hour {hour[0]+1} has {hour[1]} hits')
+    print('The rest of have none')
 
 
 def countImageHits(data):
@@ -66,9 +122,7 @@ def findPopularBrowser(data):
 def main(url):
     print(f"Running main with URL = {url}...")
     file = downloadData(url)
-    data = processData(file)
-    countImageHits(data)
-    findPopularBrowser(data)
+    processData(file)
 
 
 if __name__ == "__main__":
