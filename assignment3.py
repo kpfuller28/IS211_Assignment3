f18f207f06ad973c3748db59353e431e58fc8f05
kpfuller28 <kpfuller28@gmail.com>
2024-09-12 21:05:19 -0400

    cleaned up unused functions and added more comments

--- assignment3.py
+++ assignment3.py
@@ -8,13 +8,7 @@ import datetime
 # other imports go here
 
 
-def getData(url):
-    response = urllib.request.urlopen(url)
-    lines = [l.decode("utf-8") for l in response.readlines()]
-    cr = csv.reader(lines)
 
-    for row in cr:
-        print(row)
 
 
 def downloadData(url):
@@ -82,41 +76,41 @@ def processData(file):
             print(f'Hour {hour[0]+1} has {hour[1]} hits')
     print('The rest of have none')
 
+# THOUGHTS: I originally had separate function for different processes eg to count the image hits and find out the most popular browser. I think that this more modular set up is better in that it is more reusable, but it ends up looping through all the data for every single function. Putting all the functionality in one process data function is more efficient for the assignment, but is not as future proof or easily reusable.
+# def countImageHits(data):
+#     pattern = re.compile(r"\.(jpg|gif|png)$", re.IGNORECASE)
+#     imageHits = 0
+#     for hit in data:
+#         if pattern.search(hit[0]):
+#             imageHits += 1
+#     imagePercentage = imageHits / len(data) * 100
+#     print(f"Image requests account for {imagePercentage}% of all requests")
 
-def countImageHits(data):
-    pattern = re.compile(r"\.(jpg|gif|png)$", re.IGNORECASE)
-    imageHits = 0
-    for hit in data:
-        if pattern.search(hit[0]):
-            imageHits += 1
-    imagePercentage = imageHits / len(data) * 100
-    print(f"Image requests account for {imagePercentage}% of all requests")
 
+# def findPopularBrowser(data):
+#     pattern = re.compile(
+#         r"(Firefox|MSIE|Trident|Chrome|(?<!Chrome)Safari)", re.IGNORECASE
+#     )
+#     browserCount = {"Firefox": 0, "Internet Explorer": 0, "Chrome": 0, "Safari": 0}
+#     for hit in data:
+#         userAgentString = hit[2]
+#         browser = pattern.search(userAgentString).group()
+#         if browser == "Firefox":
+#             browser = "Firefox"
+#             browserCount[browser] += 1
+#         elif browser == "MSIE" or browser == "Trident":
+#             browser = "Internet Explorer"
+#         elif browser == "Chrome":
+#             browser = "Chrome"
+#         elif browser == "Safari":
+#             broswer = "Safari"
+#         browserCount[browser] += 1
 
-def findPopularBrowser(data):
-    pattern = re.compile(
-        r"(Firefox|MSIE|Trident|Chrome|(?<!Chrome)Safari)", re.IGNORECASE
-    )
-    browserCount = {"Firefox": 0, "Internet Explorer": 0, "Chrome": 0, "Safari": 0}
-    for hit in data:
-        userAgentString = hit[2]
-        browser = pattern.search(userAgentString).group()
-        if browser == "Firefox":
-            browser = "Firefox"
-            browserCount[browser] += 1
-        elif browser == "MSIE" or browser == "Trident":
-            browser = "Internet Explorer"
-        elif browser == "Chrome":
-            browser = "Chrome"
-        elif browser == "Safari":
-            broswer = "Safari"
-        browserCount[browser] += 1
+#     mostPopular = max(browserCount, key=browserCount.get)
 
-    mostPopular = max(browserCount, key=browserCount.get)
-
-    print(
-        f"The most popular browser is {mostPopular} with {max(browserCount.values())} hits"
-    )
+#     print(
+#         f"The most popular browser is {mostPopular} with {max(browserCount.values())} hits"
+#     )
 
 
 def main(url):
